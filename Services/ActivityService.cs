aced92c0f72b947b9ab353bd948cba28c09f2b6c
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2022-02-18 08:54:07 +0600

    Service Added

---
+++ Services/ActivityService.cs
@@ -0,0 +1,17 @@
+﻿using Shawpnojatra_Foundation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shawpnojatra_Foundation.Services
+{
+	public class ActivityService
+	{
+		public ApplicationContext _db { get; set; }
+		public ActivityService(ApplicationContext db)
+		{
+			_db = db;
+		}
+	}
+}
