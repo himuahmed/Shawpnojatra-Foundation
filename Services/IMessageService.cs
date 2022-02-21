8f1c671e68ba92ce855c49b7a1b811bef735932c
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2022-02-21 12:50:18 +0600

    Message API added,Layoute changes

---
+++ Services/IMessageService.cs
@@ -0,0 +1,19 @@
+﻿
+using Shawpnojatra_Foundation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shawpnojatra_Foundation.Services
+{
+	public interface IMessageService
+	{
+		IEnumerable<Message> GetMessages();
+		Message GetMessageByID(int ID);
+		int AddMessage(Message message);
+
+		int UpdateMessage(Message message);
+		int DeleteMessage(int ID);
+	}
+}
