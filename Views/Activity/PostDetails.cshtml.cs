1c16b18b03eeb462ac1798990323b2afd63bac9d
Himu <mohsi.ahamed@gmail.com>
2022-03-01 14:43:59 +0600

    Show posts and post cards Done

---
+++ Views/Activity/PostDetails.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shawpnojatra_Foundation.Views.Activity
+{
+    public class PostDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
